3182869b7541b24d37ec9e7cb92534b7d807a790
llem00n <alex.porubaimikh@gmail.com>
2023-10-19 11:57:47 +0300

    add initial layout

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labi-le/hyprland-ipc-client"
 	"log"
 	"os"
+	"os/exec"
 )
 
 type Output struct {
@@ -23,7 +24,37 @@ type ed struct {
 	client.DummyEvHandler
 }
 
+func ReadFirstLayout() {
+	devices := map[string][]map[string]any{}
+	out, err := exec.Command("hyprctl", "devices", "-j").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal(out, &devices); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices["keyboards"] {
+		if device["main"] == true {
+			text, ok := device["active_keymap"].(string)
+			if !ok {
+				log.Fatal("Could not read active keymap")
+			} else {
+				ToStdOut(Output{ 
+					Text:    text,
+					Tooltip: "Current keyboard layout",
+					Class:   "keyboard-layout",
+				})
+			}
+
+			
+		}
+	}
+}
+
 func main() {
+	ReadFirstLayout()
 	c := client.NewClient(os.Getenv("HYPRLAND_INSTANCE_SIGNATURE"))
 	e := &ed{}
 	client.Subscribe(c, e, client.EventActiveLayout)
